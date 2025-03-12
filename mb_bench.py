c7e24bc3a23fa4b7f8af469a2564329ddb8bb1bf
hvermaq <hvarma2@gmail.com>
2025-03-12 17:17:59 +0800

    outline

---
+++ mb_bench.py
@@ -0,0 +1,8 @@
+#many body benchmark
+#for systems with ground state energies known in the thermodynamic limit
+# typically we need cases where the energy converges to a finite value in the thermodynamic limit
+# random seeds yeild the error bars at individual points
+#structure: MB_benchmark class.
+#inputs: nqbits, nshots, noise param, rnds
+#selections: Anstaz, topology, compression, 2x hardwares, gateset 
+#next version: noise type, 
